289768f55a6f2815106ec651d328f67a2eb0c5ae
Osten Wiecen  Jap <58747639+ostenjap@users.noreply.github.com>
2023-03-10 23:15:15 +0100

    twitter and git-added

--- src/exportLinktree.js
+++ src/exportLinktree.js
@@ -6,9 +6,12 @@ function Linktree() {
   const [links] = useState([
     { name: 'Instagram', url: 'https://www.instagram.com/ostenjap/' },
     { name: 'LinkedIn', url: 'https://www.linkedin.com/in/osten-wiecen-jap-649175179/' },
-    { name: ' E-Mail ', url: 'mailto:wiecen@gmail.com' },
-    { name: 'Phone', url: 'tel:017666531672'}
+    { name: 'E-Mail', url: 'mailto:wiecen@gmail.com' },
+    { name: 'Phone', url: 'tel:017666531672'},
+    { name: 'Twitter', url: 'https://twitter.com/OstenJap'},
+    { name: 'GitHub', url: 'https://github.com/ostenjap'}
   ]);
+
 
   return (
      <div className="linktree-container">
